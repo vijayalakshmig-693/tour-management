6744528066d5202a1068dcf7a10d513a7d1b6dbe
pknowledge <xyz@gmail.com>
2023-11-04 17:25:58 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,34 @@
+// This is a comment in JavaScript
+// You can write comments like this to provide explanations in your code.
+
+// Declare and initialize a variable
+let greeting = "Hello, World!";
+
+// Display the greeting in the browser's console
+console.log(greeting);
+
+// Define a function
+function sayHello(name) {
+  console.log("Hello, " + name + "!");
+}
+
+// Call the function
+sayHello("Alice");
+
+// Create an array
+let fruits = ["apple", "banana", "cherry"];
+
+// Loop through the array and display each fruit
+for (let i = 0; i < fruits.length; i++) {
+  console.log("Fruit: " + fruits[i]);
+}
+
+// Use the 'if' statement to make a decision
+let number = 5;
+if (number > 0) {
+  console.log(number + " is a positive number.");
+} else if (number < 0) {
+  console.log(number + " is a negative number.");
+} else {
+  console.log(number + " is zero.");
+}
